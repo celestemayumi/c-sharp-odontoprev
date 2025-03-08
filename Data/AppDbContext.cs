3f71cdbe63bc0a6d2173423bdd2b86f63a967408
Celeste Tanaka <cehmayumi@gmail.com>
2025-03-07 23:18:05 -0300

    fix: correcao na conexao do banco de dados

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -4,5 +4,6 @@ namespace c_sharp_odontoprev.Data
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     }
 }
